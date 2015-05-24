2e6b73c3bff07000ca7ce8f7ccabab6439e86131
skurella <s.kasyap@wp.pl>
2015-05-24 14:44:42 +0100

    Add firmware I2C support
    Waveforms look good on the scope, not yet tested with RTC

--- software/MCUtest.X/nixie.c
+++ software/MCUtest.X/nixie.c
@@ -9,6 +9,7 @@
 #include "nixie.h"
 
 volatile unsigned char ENCODER_STATE;
+volatile unsigned char I2C_STATE;
 
 // Init encoder inputs
 void encoder_init() {
@@ -122,3 +123,116 @@ unsigned char encoder_updateState() {
 
     return ENCODER_TURN_NOTURN;
 }
+
+// Init I2C
+void i2c_init() {
+    // Disable SSP module
+    SSPCONbits.SSPEN = 0;
+    
+    // Clear output latches
+    //I2C_DATA_PIN = 0; // for some reason these do not work as expected
+    //I2C_CLOCK_PIN = 0;
+    RC3 = 0;
+    RC4 = 0;
+
+    // Set both lines (idle)
+    I2C_DATA_SET;
+    I2C_CLOCK_SET;
+
+    I2C_STATE = I2C_STATE_IDLE;
+
+    // Configure Timer0
+    OPTION_REGbits.T0CS = 0; // Timer0 clock source: instruction cycle clock
+    OPTION_REGbits.PSA = 1; // assign prescaler to watchdog
+}
+
+// I2C delay (minimum 5us = 10 instruction cycles)
+// Timer0 is used to handle interrupts
+inline void i2c_delay() {
+    i2c_begindelay();
+    i2c_enddelay();
+}
+
+inline void i2c_begindelay() {
+    TMR0 = 0;
+    INTCONbits.TMR0IF = 0;
+}
+
+inline void i2c_enddelay() {
+    while (TMR0 < 10 && !INTCONbits.TMR0IF);
+}
+
+void i2c_start() {
+    // If I2C is already started, do a restart
+    if (I2C_STATE != I2C_STATE_IDLE) {
+        I2C_DATA_SET;
+        i2c_delay();
+        I2C_CLOCK_SET;
+        while (I2C_CLOCK_PIN == 0); // clock stretching
+        i2c_delay();
+    }
+
+    I2C_DATA_CLEAR;
+    i2c_delay();
+    I2C_CLOCK_CLEAR;
+
+    I2C_STATE = I2C_STATE_ACTIVE;
+}
+
+void i2c_stop() {
+    I2C_DATA_CLEAR;
+    i2c_delay();
+    I2C_CLOCK_SET;
+    while (I2C_CLOCK_PIN == 0); // clock stretching
+    i2c_delay();
+    I2C_DATA_SET;
+    i2c_delay();
+
+    I2C_STATE = I2C_STATE_IDLE;
+}
+
+void i2c_write_bit(char b) {
+    if (b) {
+        I2C_DATA_SET;
+    } else {
+        I2C_DATA_CLEAR;
+    }
+    i2c_delay();
+    I2C_CLOCK_SET;
+    while (I2C_CLOCK_PIN == 0); // clock stretching
+    // SCL is high, data is valid
+    i2c_delay();
+    I2C_CLOCK_CLEAR;
+}
+
+char i2c_read_bit() {
+    char b;
+    I2C_DATA_SET;
+    i2c_delay();
+    I2C_CLOCK_SET;
+    while (I2C_CLOCK_PIN == 0); // clock stretching
+    // SCL is high, data is valid
+    b = I2C_DATA_PIN;
+    i2c_delay();
+    I2C_CLOCK_CLEAR;
+    return b;
+}
+
+// Returns 0 if acknowledged by the slave
+char i2c_write_byte(char byte) {
+    for (char i = 0; i < 8; ++i) {
+        i2c_write_bit((byte & 0x80) != 0);
+        byte <<= 1;
+    }
+    return i2c_read_bit();
+}
+
+char i2c_read_byte(char nack) {
+    char byte = 0;
+    for (char i = 0; i < 8; ++i) {
+        byte = (byte << 1) | i2c_read_bit();
+    }
+    i2c_write_bit(nack);
+    return byte;
+}
+
--- software/MCUtest.X/nixie.h
+++ software/MCUtest.X/nixie.h
@@ -12,6 +12,8 @@
 extern "C" {
 #endif
 
+#include <xc.h>
+
 #define ENCODER_STATE_DENT      0
 #define ENCODER_STATE_BEGINCW   1
 #define ENCODER_STATE_MIDCW     2
@@ -41,6 +43,39 @@ inline void encoder_setState(unsigned char state);
 // 1 - clockwise turn
 // 2 - counterclockwise turn
 unsigned char encoder_updateState();
+
+
+
+#define I2C_DATA_OUTPUT    TRICSbits.TRISC4;
+#define I2C_DATA_CLEAR     TRISCbits.TRISC4 = 0; // pull SDA low
+#define I2C_DATA_SET       TRISCbits.TRISC4 = 1; // let SDA high
+#define I2C_DATA_PIN       RC4         // SDA pin
+
+#define I2C_CLOCK_OUTPUT   TRISCbits.TRISC3;
+#define I2C_CLOCK_CLEAR    TRISCbits.TRISC3 = 0; // pull SCL low
+#define I2C_CLOCK_SET      TRISCbits.TRISC3 = 1; // let SCL high
+#define I2C_CLOCK_PIN      RC3         // SCL pin
+
+#define I2C_STATE_IDLE      0
+#define I2C_STATE_ACTIVE    1
+
+extern volatile unsigned char I2C_STATE;
+
+// Init I2C
+void i2c_init();
+
+// I2C delay (minimum 5us)
+// Timer0 is used in order to handle interrupts
+inline void i2c_delay();
+inline void i2c_begindelay();
+inline void i2c_enddelay();
+
+void i2c_start();
+void i2c_stop();
+void i2c_write_bit(char b);
+char i2c_read_bit();
+char i2c_write_byte(char byte); // Returns 0 if acknowledged by the slave
+char i2c_read_byte(char nack);
 
 #ifdef	__cplusplus
 }
